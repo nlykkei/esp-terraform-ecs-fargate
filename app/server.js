75f6456217aabf8aca7c3cf8b05974c0aa041388
Nicolas Lykke Iversen <nicolas.lykke.iversen@lego.com>
2023-01-13 17:24:02 +0100

    Added node-fetch to app

--- app/server.js
+++ app/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
 const fs = require("fs");
+const fetch = require("node-fetch");
 
 const PORT = process.env.PORT || 8080;
 const APP_URL = process.env.APP_URL || "localhost";
